eb06d5e4a8f7d655b5030ff06ddc3ea3fb26ddfe
Ben Klein <robobenklein@gmail.com>
2018-03-21 16:13:04 -0400

    New cpuclock script

--- bin/cpu_clock.sh
+++ bin/cpu_clock.sh
@@ -1,9 +1,37 @@
 #!/bin/zsh
 
-echo "CPU Freq Range $(cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_min_freq | uniq) - $(cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_max_freq | uniq)"
-echo "Scaling Range $(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq | uniq) - $(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq | uniq)"
+hwrange="$(($(cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_min_freq | uniq) / 1000 )) - $(($(cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_max_freq | uniq) / 1000 ))"
+scalerange="$(($(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq | uniq) / 1000)) - $(($(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq | uniq) / 1000))"
+
+eval "$(resize)"
 
 # prompt to set clockspeed
-echo -n "New clockspeed? : "
-read new_clock
-echo $new_clock | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
+if [ -z "$cpuprog" ]; then
+cpuprog=$(whiptail --title "Choose Action" \
+  --menu "Hardware: ${hwrange} Scaling: ${scalerange}" \
+  $(( LINES - 4 )) $(( COLUMNS - 18 )) $(( $LINES - 12 )) \
+  "cancel" "Do nothing." \
+  "set_minfreq" "Set the minimum scale frequency." \
+  "set_maxfreq" "Build the rip_rpi Pi emulator and dependency container." \
+  3>&1 1>&2 2>&3
+)
+fi
+
+case $cpuprog in
+  set_minfreq)
+    echo -n "New min clockspeed? (MHz) : "
+    read new_clock
+    echo $(($new_clock * 1000)) | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
+    ;;
+  set_maxfreq)
+    echo -n "New max clockspeed? (MHz) : "
+    read new_clock
+    echo $(($new_clock * 1000)) | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
+    ;;
+  cancel)
+    ;;
+esac
+
+echo "HWRANGE $hwrange"
+echo "SFRANGE $scalerange"
+
