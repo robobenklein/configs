073b42181a92207779a16f1cc6087c81a7ce1951
Ben Klein <robobenklein@gmail.com>
2018-05-30 11:38:43 -0400

    Work on lslbntl

--- zsh/plunks/lslbntl.zsh
+++ zsh/plunks/lslbntl.zsh
@@ -3,13 +3,16 @@
 
 # complicated fix to not show too many dotfiles for just 'l'
 # does not show hidden files if the terminal height can't show everything
+# TODO figure out how to avoid running _ALL command twice if possible
 function lslbntl() {
-  local Z_TMP_LS_DOTSHOWN_SIZE=$($Z_LSBASE $Z_LSARGEXTRA -lah $@ 2>/dev/null | wc -l)
-  if [[ $Z_TMP_LS_DOTSHOWN_SIZE -gt $(tput lines) ]]; then # then don't show hidden files
+  local Z_LSARGS="-lh"
+  local Z_LSARGS_ALL="-lah"
+  local Z_TMP_LS_DOTSHOWN=$($Z_LSBASE $Z_LSARGEXTRA $Z_LSARGS_ALL "$@" 2>&1 )
+  local Z_TMP_LS_DOTSHOWN_SIZE=$(echo "$Z_TMP_LS_DOTSHOWN" | wc -l)
+  if [[ "$Z_TMP_LS_DOTSHOWN_SIZE" -gt "$(tput lines)" ]]; then # then don't show hidden files
     echo "Not showing hidden files"
-    Z_LSARGS="-lh"
+    $Z_LSBASE $Z_LSARGEXTRA $Z_LSARGS "$@"
   else
-    Z_LSARGS="-lah"
+    $Z_LSBASE $Z_LSARGEXTRA $Z_LSARGS_ALL "$@"
   fi
-  $Z_LSBASE $Z_LSARGEXTRA $Z_LSARGS $@
 }
