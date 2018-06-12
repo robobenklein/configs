df818806141798db1060be6d15368b083acbe163
Ben Klein <robobenklein@gmail.com>
2018-06-12 14:57:27 -0400

    Finally working out proper autoload on git lfs

--- zsh/plunks/git-lfs-completion.zsh
+++ zsh/plunks/git-lfs-completion.zsh
@@ -1,4 +1,5 @@
 
 # adds the "git lfs" completion
 # "git-lfs" should be autoloaded by compinit and fpath
-zstyle ':completion:*:*:git:*' user-commands lfs:'access to large file storage controls'
+# autoload -Uz _git-lfs
+# zstyle ':completion:*:*:git:*' user-commands lfs:'access to large file storage controls'
