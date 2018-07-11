28cb88f0b9d1f7aee22b33aa887faf46fc6e9f56
Ben Klein <robobenklein@gmail.com>
2018-07-11 19:13:23 -0400

    Some quick test scripts

---
+++ tests/variable-check.zsh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+N=2000000
+local SOME_VARIABLE
+SOME_VARIABLE="something"
+
+TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S'
+
+echo
+echo "typeset -p:"
+time (repeat $N { typeset -p "SOME_VARIABLE" > /dev/null 2>&1 })
+
+echo
+echo '${+var}:'
+time (repeat $N { (( ${+SOME_VARIABLE} )) })
+
+echo
+echo '[[ -v var ]]'
+time (repeat $N { [[ -v SOME_VARIABLE ]] })
+
+echo
+echo "Variable NOT set: "
+unset SOME_VARIABLE
+
+echo
+echo "typeset -p:"
+time (repeat $N { typeset -p "SOME_VARIABLE" > /dev/null 2>&1 })
+
+echo
+echo '${+var}:'
+time (repeat $N { (( ${+SOME_VARIABLE} )) })
+
+echo
+echo '[[ -v var ]]'
+time (repeat $N { [[ -v SOME_VARIABLE ]] })
