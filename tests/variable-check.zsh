bab0cee68a47f6e1729ed7a3d27292cfe7f3a880
Ben Klein <robobenklein@gmail.com>
2018-07-12 14:23:38 -0400

    Found a new defined function for p9k

--- tests/variable-check.zsh
+++ tests/variable-check.zsh
@@ -4,6 +4,15 @@ N=200000
 local SOME_VARIABLE
 SOME_VARIABLE="something"
 
+defined () {
+	local varname="$1"
+	typeset -p "$varname" > /dev/null 2>&1
+}
+
+def () {
+  [[ ! -z "${(tP)1}" ]]
+}
+
 TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S'
 
 echo
@@ -17,6 +26,14 @@ time (repeat $N { (( ${+SOME_VARIABLE} )) })
 echo
 echo '[[ -v var ]]'
 time (repeat $N { [[ -v SOME_VARIABLE ]] })
+
+echo
+echo 'p9k old defined'
+time (repeat $N { defined SOME_VARIABLE })
+
+echo
+echo 'test def function'
+time (repeat $N { def SOME_VARIABLE })
 
 echo
 echo "Variable NOT set: "
@@ -33,3 +50,11 @@ time (repeat $N { (( ${+SOME_VARIABLE} )) })
 echo
 echo '[[ -v var ]]'
 time (repeat $N { [[ -v SOME_VARIABLE ]] })
+
+echo
+echo 'p9k old defined'
+time (repeat $N { defined SOME_VARIABLE })
+
+echo
+echo 'test def function'
+time (repeat $N { def SOME_VARIABLE })
