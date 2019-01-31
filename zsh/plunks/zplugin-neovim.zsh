54ff3ad44ee54518d1c8330bc1825d1a313b5695
Ben Klein <robobenklein@gmail.com>
2019-01-31 18:11:17 -0500

    Moved neovim installer

--- zsh/plunks/zplugin-neovim.zsh
+++ zsh/plunks/zplugin-neovim.zsh
@@ -1,11 +1,23 @@
 
-if (( ! ${+commands[nvim]} )) || [[ ! -z ZSHRC_MANAGE_NVIM ]]; then
+function zplg-setup-neovim () {
   # neovim linux appimage on x64
   if [[ $unamearch == "x86_64" ]]; then
     if [[ $unamestr == "Linux" ]]; then
       zplugin ice nocompletions from"gh-r" bpick"*.appimage" \
         as"program" mv"nvim.appimage -> nvim" pick"nvim" id-as'neovim'
       zplugin load neovim/neovim
+    elif [[ $unamestr == "Darwin" ]]; then
+      # not yet implemented
     fi
   fi
-fi
+
+  function zplg-remove-neovim () {
+    zplugin unload neovim
+    rm -rf ${ZPLGM[PLUGINS_DIR]}/neovim
+    rehash
+  }
+}
+# manage it if it's already there
+[[ -d ${ZPLGM[PLUGINS_DIR]}/neovim ]] && {
+  zplg-setup-neovim
+}
