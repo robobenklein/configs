1a62b029d518e8ed384aa54cb5e79b5fab8ae7f6
Ben Klein <robobenklein@gmail.com>
2019-03-17 01:24:22 -0400

    Twas about time I made these available

---
+++ bin/byzanz-record-region-gui.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# AUTHOR:   (c) Rob W 2012, modified by MHC (http://askubuntu.com/users/81372/mhc)
+# NAME:     GIFRecord 0.1
+# DESCRIPTION:  A script to record GIF screencasts.
+# LICENSE:  GNU GPL v3 (http://www.gnu.org/licenses/gpl.html)
+# DEPENDENCIES:   byzanz,gdialog,notify-send (install via sudo add-apt-repository ppa:fossfreedom/byzanz; sudo apt-get update && sudo apt-get install byzanz gdialog notify-osd)
+
+# Time and date
+TIME=$(date +"%Y-%m-%d_%H%M%S")
+
+# Delay before starting
+DELAY=10
+
+# Standard screencast folder
+FOLDER="/tmp/gif-recordings"
+mkdir -p /tmp/gif-recordings
+
+# Default recording duration
+DEFDUR=10
+
+# Sound notification to let one know when recording is about to start (and ends)
+beep() {
+    paplay /usr/share/sounds/freedesktop/stereo/message-new-instant.oga &
+}
+
+# Custom recording duration as set by user
+USERDUR=$(gdialog --title "Duration?" --inputbox "Please enter the screencast duration in seconds" 200 100 2>&1)
+
+# Duration and output file
+if [ $USERDUR -gt 0 ]; then
+    D=$USERDUR
+else
+    D=$DEFDUR
+fi
+
+# Region geometry
+slop=$(slop -f "%x %y %w %h %g %i") || exit 1
+read -r X Y W H G ID < <(echo $slop)
+
+# Notify the user of recording time and delay
+notify-send "GIFRecorder" "Recording duration set to $D seconds. Recording will start in $DELAY seconds."
+
+#Actual recording
+sleep $DELAY
+beep
+byzanz-record -c --verbose --delay=0 --duration=$D --x=$X --y=$Y --width=$W --height=$H "$FOLDER/GIFrecord_$TIME.gif"
+beep
+
+# Notify the user of end of recording.
+notify-send "GIFRecorder" "Screencast saved to $FOLDER/GIFrecord_$TIME.gif"
+
