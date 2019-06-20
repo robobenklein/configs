7a6f59a8ce548b71010be80a732d9f405c188c5a
Ben Klein <robobenklein@gmail.com>
2019-06-20 19:17:30 -0400

    Use tm more like an alias

--- zsh/plunks/fzy-tmux.zsh
+++ zsh/plunks/fzy-tmux.zsh
@@ -3,7 +3,7 @@
 # original idea from
 # http://owen.cymru/fzf-ripgrep-navigate-with-bash-faster-than-ever-before/
 
-tm() {
+tm () {
   [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
   if (( ${+1} )); then
     tmux $change -t "$1" 2>/dev/null || {
@@ -11,14 +11,18 @@ tm() {
     }
   else
     local session
-    session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzy -p 'tmux sessions > ' ) && {
-      tmux $change -t "$session"
-    } || {
-      [[ ! -n "$session" ]] && return
-      read -q "REPLY?Create session \"${session}\"? [y/N] "
-      echo
-      [[ $REPLY == 'y' ]] && tm $session
-    }
+    if [[ "$(tmux list-sessions -F "#{session_name}" 2>/dev/null)" != "" ]]; then
+      session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzy -p 'tmux sessions > ' ) && {
+        tmux $change -t "$session"
+      } || {
+        [[ ! -n "$session" ]] && return
+        read -k 1 "REPLY?Create session \"${session}\"? [Y/n] "
+        echo
+        [[ $REPLY == 'y' || $REPLY == "" ]] && tm $session
+      }
+    else
+      tmux
+    fi
   fi
 }
 
