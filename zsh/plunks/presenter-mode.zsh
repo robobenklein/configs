a5e547ce4e8a7b735d96a6e98094517a2f17569a
Ben Klein <robobenklein@gmail.com>
2019-10-01 15:11:13 -0400

    update the presenter mode with new features

--- zsh/plunks/presenter-mode.zsh
+++ zsh/plunks/presenter-mode.zsh
@@ -5,17 +5,31 @@ function zsh_presenter_mode_start() {
   # (e.x. shows aliases in expanded form so peeps aren't lost)
   function preshow_expanded_command() {
     if [[ "$1" != "$2" ]]; then
-      printf "%$(($(tput cols)))b\n" "expanded: \033[0;1m$2\033[0;0m"
+      printf "%$(($(tput cols)))b\n" "\033[0;2mexpanded: \033[0;1m$2\033[0;0m"
     fi
+    previous_command="$2"
+    precmd_functions+=(postshow_previous_command)
+  }
+  function postshow_previous_command() {
+      # a=("${(@)a:#b}")
+      printf "%$(($(tput cols)))b\n" "\033[0;2mprevious: \033[0;1m$previous_command\033[0;0m"
+      precmd_functions=("${(@)precmd_functions:#postshow_previous_command}")
   }
   Z_PRESENTER_PREEXEC_FUNCTIONS_BACKUP=$preexec_functions
+  Z_PRESENTER_PRECMD_FUNCTIONS_BACKUP=$precmd_functions
   echo "Backed up preexec array: $Z_PRESENTER_PREEXEC_FUNCTIONS_BACKUP"
+  echo "Backed up precmd array: $Z_PRESENTER_PRECMD_FUNCTIONS_BACKUP"
   preexec_functions+=(preshow_expanded_command)
+  precmd_functions+=(postshow_previous_command)
   Z_PRESENTER_MODE_ENABLED=1
   # set terminal background to black to improve contrast:
   echo -e "\033]11;#000000\a"
   # switch toggle command
   alias zsh-presenter-toggle=zsh_presenter_mode_stop
+  # load the as-you-type expansion:
+  zplugin ice from"gh"
+  zplugin load "simnalamburt/zsh-expand-all"
+  ZSH_EXPAND_ALL_DISABLE=word
   sleep 1
   clear
   echo ""
@@ -28,10 +42,14 @@ function zsh_presenter_mode_stop() {
   # Check that we're not gonna accidentally set the preexec array blank
   if (( $+Z_PRESENTER_MODE_ENABLED )) && (( $+Z_PRESENTER_PREEXEC_FUNCTIONS_BACKUP )); then
     preexec_functions=($Z_PRESENTER_PREEXEC_FUNCTIONS_BACKUP)
+    precmd_functions=($Z_PRESENTER_PRECMD_FUNCTIONS_BACKUP)
     echo "restored preexec array: $preexec_functions"
+    echo "restored precmd array: $precmd_functions"
     unset Z_PRESENTER_MODE_ENABLED
     unset Z_PRESENTER_PREEXEC_FUNCTIONS_BACKUP
+    unset Z_PRESENTER_PRECMD_FUNCTIONS_BACKUP
     alias zsh-presenter-toggle=zsh_presenter_mode_start
+    zplugin unload "simnalamburt/zsh-expand-all"
   else
     >&2 echo "Presenter mode not enabled."
     return 1
