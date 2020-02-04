83449e9986bbc28f2fa1bcacd3ba44db6873ad05
Ben Klein <robobenklein@gmail.com>
2020-02-04 02:19:20 -0500

    gsu on provision

--- provision.sh
+++ provision.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# curl -fsSL https://raw.githubusercontent.com/robobenklein/configs/master/provision.sh | bash
 NOW="$(date --rfc-3339=date)"
 mkdir -p ~/code
 pushd ~/code
 git clone https://github.com/robobenklein/configs.git
 pushd configs
+git submodule update --init --recursive
 ./install
 popd
 popd
