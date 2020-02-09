0194cda33c8452bea4804e6f14f1849a2fe6f154
Ben Klein <robobenklein@gmail.com>
2020-02-09 02:18:54 -0500

    use provision script

--- provision.sh
+++ provision.sh
@@ -3,7 +3,9 @@
 NOW="$(date --rfc-3339=date)"
 mkdir -p ~/code
 pushd ~/code
-git clone https://github.com/robobenklein/configs.git
+[[ ! -d configs ]] && {
+  git clone https://github.com/robobenklein/configs.git
+}
 pushd configs
 git submodule update --init --recursive
 ./install
