866921644accf3ed6fa31655e20b1365e4b8c46c
Ben Klein <robobenklein@gmail.com>
2020-04-16 05:48:24 -0400

    discord normal deb in comment

--- sbin/discord-canary.zsh
+++ sbin/discord-canary.zsh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 wget 'https://discordapp.com/api/download/canary?platform=linux' -O /tmp/discord-canary.deb
+#wget 'https://discordapp.com/api/download?platform=linux&format=deb' -O /tmp/discord.deb
 sudo apt install /tmp/discord-canary.deb
