b9f09ba97e1dfc1d0da1ecbce736cd61ccfac40a
Ben Klein (on d13vm) <robobenklein@gmail.com>
2026-04-06 16:36:41 -0400

    set a few kde configs

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -7,5 +7,15 @@
     shortcuts = {
       kwin.Overview = ["Meta" "Meta+Space" "Meta+W"];
     };
+    configFile = {
+      kwinrc."org.kde.kdecoration2".ButtonsOnLeft = "X";
+      kwinrc."org.kde.kdecoration2".ButtonsOnRight = "";
+      kwinrc.EdgeBarrier.EdgeBarrier = 0;
+      kdeglobals.KDE.SingleClick = true;
+      kded5rc.Module-device_automounter.autoload = false;
+
+      # TODO where to install the file to
+      kwinrc.Plugins.dynamic_workspacesEnabled = true;
+    };
   };
 }
