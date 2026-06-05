e3b3acbf4aa24633618e684c04de442982a5bd95
Ben Klein <robobenklein@gmail.com>
2026-06-05 05:24:27 -0400

    borg cube

--- sbin/root-borg-backup.zsh
+++ sbin/root-borg-backup.zsh
@@ -1,9 +1,10 @@
 #!/bin/zsh
+# only good for borg v1
 
 repo_uri="$1"
 shift
 target_paths=($@)
-extra_opts=( --stats --progress --exclude '/home/*/.cache' --exclude '/root/.cache' )
+extra_opts=( --stats --progress --exclude '/home/*/.cache' --exclude '/root/.cache' --exclude '/home/*/.local/share/Trash' )
 borg_cmd=${BORG_COMMAND:-borg}
 
 if [[ -z $repo_uri ]]; then
@@ -19,10 +20,10 @@ fi
   archive_name="${(j.,.)path_names}"
 } || {
   archive_name="robo-system-script"
-  extra_opts+=( --one-file-system )
-  target_paths=( /boot /etc /home /opt /root /srv /usr /var )
+  #extra_opts+=( --one-file-system )
+  target_paths=( /boot /etc /home /opt /root /srv /usr )
   # some paths are just links into /usr (on some systems)
-  if_present_paths=( /bin /lib /lib32 /lib64 /libx32 /sbin )
+  if_present_paths=( /bin /lib /lib32 /lib64 /libx32 /sbin /var/lib /var/www /var/mail /var/log /var/backups )
   for dir in $if_present_paths; do
     [[ -e $dir ]] && target_paths+=("$dir")
   done
@@ -32,7 +33,7 @@ echo "Target paths: ${target_paths}"
 echo "Archive name: ${archive_name}"
 echo "Borg version: $($borg_cmd --version)"
 
-cmd=(sudo nice $borg_cmd create ${extra_opts} "${repo_uri}"'::{hostname}-{now:%Y-%m-%d}-'"${archive_name//\//-}" ${target_paths})
+cmd=(sudo nice $borg_cmd create ${extra_opts} "${repo_uri}"'::{hostname}-{now}-'"${archive_name//\//-}" ${target_paths})
 echo "$cmd"
 printf '%s' 'Run command? [y/n]'
 
